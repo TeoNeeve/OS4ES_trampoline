0c72b1433d86d85b5ced12b6fa0e289e39d30dc9
TeoNeeve <vecchia.teo@gmail.com>
2025-10-29 12:43:17 +0100

    debugging, sill problems

--- labsPOLITO/lab02/ex1/ex1.c
+++ labsPOLITO/lab02/ex1/ex1.c
@@ -4,9 +4,9 @@
 #define LED_PIN 13 
 #define pinSwitch 12
 #define Switch_THRESHOLD 1000   // milliseconds
-#define ADC_10BIT_MASK 0x3FF    // Mask for 10-bit ADC (bits 0–9)
-#define PRESS_FLAG_BIT 12       // Bit position for press flag
 #define DEBUG_PIN 10            // Debug output pin ######################################
+#define PRESS_FLAG_BIT 12       // Bit position for press flag
+#define ADC_10BIT_MASK 0b001111111111 // Mask for 10-bit ADC (bits 0–9)
 
 // extern int my_time; usarlo non funziona 
 
@@ -68,7 +68,7 @@ int timer_pressure(void)
     int message = A0_valueADC & ADC_10BIT_MASK;  // bits 0–9: raw ADC
     // Only set the press flag once per long press (edge), not continuously
     if (long_pressed_flag && !long_press_sent) {
-        message |= (1 << PRESS_FLAG_BIT);        // bit 12
+        message |= (1 << PRESS_FLAG_BIT); // setting bit 12 = 1
         long_press_sent = true; // mark as sent
     }
     return message;
@@ -76,26 +76,27 @@ int timer_pressure(void)
 
 int message_scheduler(int received_message) // Extracting on the receiver side:
 {
-    static int scheduled_message = -1;
     static int reference_value = -1;
+    int scheduled_message = -1;
     int received_adc_value = received_message & ADC_10BIT_MASK; // Value of A0, bits 0..9
+    int received_voltage_mV = (received_adc_value * 5000) / 1023; // in mV
 
-    // If the press flag is set, update the reference and do not send a
-    // blink command to TaskV (return -1 -> no SendMessage from TaskM).
+    // If the press flag is set, update the reference and turn LED OFF
     if (received_message & (1 << PRESS_FLAG_BIT)) {
-        reference_value = received_adc_value; // Set new reference value
-        return 0; // Refecence just set, turn LED OFF
+        reference_value = received_voltage_mV;
+        return 0;
     }
 
-    // If we don't have a reference yet, ask TaskV to turn LED ON (code 3)
+    // If we don't have a reference yet turn LED ON
     if (reference_value == -1) {
-        return 3; // LED ON (no reference yet)
+        return 3;
     }
-
-    int difference = abs(received_adc_value - reference_value);
+    
+    int difference = abs(received_voltage_mV - reference_value);
     if (difference < 100) {
         scheduled_message = 0; // LED OFF
-    } else if (difference < 200) {
+        // per qualche motivo continua ad entrare qua dopo la ref settata
+    } else  if (difference < 200) {
         scheduled_message = 1; // Blink slow
     } else {
         scheduled_message = 2; // Blink fast
