ec967b28f2de7e1a80383fe120cf5798a5504b1c
TeoNeeve <vecchia.teo@gmail.com>
2025-11-17 19:00:14 +0100

    iozsdio

---
+++ labsPOLITO/lab03/ex3/ex3.cpp
@@ -0,0 +1,147 @@
+#include <Arduino.h>
+#include "tpl_os.h"
+#include <avr/wdt.h>
+
+#define	A_WCET		200
+#define	B_WCET		700
+#define	C_WCET		300
+
+#define	A_PERIOD	1000
+#define	B_PERIOD	1500
+#define	C_PERIOD	2800
+
+DeclareAlarm(activateA);
+DeclareAlarm(activateB);
+DeclareAlarm(activateC);
+
+
+void setup(void)
+{
+	Serial.begin(115200);
+	delay(100);  // Give serial time to initialize
+	Serial.println("System starting...");
+    StartOS(OSDEFAULTAPPMODE);
+}
+
+void loop(void)
+{
+    while(1){
+        // Task implementations used by the OIL file
+    }
+}
+
+TASK(InitTask)
+{
+    SendMessage(CriticalMessage, 0);
+    TerminateTask();
+}
+
+void do_things( int ms )
+{
+	unsigned long mul = ms * 504UL;
+	unsigned long i;
+	for(i=0; i<mul; i++) millis();
+}
+
+TASK(TaskA)
+{
+	static int countA = 0;
+	countA++;
+	StatusType CriticalA;
+	int deadline_A = countA * A_PERIOD;
+	int start_A = millis();
+	Serial.print("Started TaskA at ");
+	Serial.println(start_A);
+
+	do {
+    	CriticalA = ReceiveMessage(CriticalMessage);
+	} while (CriticalA != E_OK);
+
+	Serial.println("Entering critical section of TaskA");
+	do_things(A_WCET_CRITIC);
+	int end_A = millis();
+	if (end_A > deadline_A) {
+		Serial.print("TaskA missed its deadline of ");
+		Serial.print(deadline_A);
+		Serial.print(" ms (finished at ");
+		Serial.print(end_A);
+		Serial.println(" ms)");
+	}
+	else {
+		Serial.print("Finished TaskA at ");
+		Serial.println(end_A);
+	}
+	Serial.println("Exiting critical section of TaskA");
+
+	sendMessage(CriticalMessage, 0);
+	TerminateTask();
+}
+
+TASK(TaskB)
+{
+	static int countB = 0;
+	countB++;
+	int deadline_B = countB * B_PERIOD;
+	int start_B = millis();
+	Serial.print("Started TaskB at ");
+	Serial.println(start_B);
+	do_things(B_WCET);
+	int end_B = millis();
+	if (end_B > deadline_B) {
+		Serial.print("TaskB missed its deadline of ");
+		Serial.print(deadline_B);
+		Serial.print(" ms (finished at ");
+		Serial.print(end_B);
+		Serial.println(" ms)");
+	}
+	else {
+		Serial.print("Finished TaskB at ");
+		Serial.println(end_B);
+	}
+	TerminateTask();
+}
+
+TASK(TaskC)
+{
+	static int countC = 0;
+	countC++;
+	StatusType CriticalA;
+	int deadline_C = countC * C_PERIOD;
+	int start_C = millis();
+	Serial.print("Started TaskC at ");
+	Serial.println(start_C);
+	do_things(C_WCET - C_WCET_CRITIC);
+
+	do {
+    	CriticalA = ReceiveMessage(CriticalMessage);
+	} while (CriticalA != E_OK);
+
+	Serial.println("Entering critical section of TaskC");
+	do_things(C_WCET_CRITIC);
+	ReleaseResource(sharedRes);
+	int end_C = millis();
+	if (end_C > deadline_C) {
+		Serial.print("TaskC missed its deadline of ");
+		Serial.print(deadline_C);
+		Serial.print(" ms (finished at ");
+		Serial.print(end_C);
+		Serial.println(" ms)");
+	}
+	else {
+		Serial.print("Finished TaskC at ");
+		Serial.println(end_C);
+	}
+	Serial.println("Exiting critical section of TaskC");
+
+	sendMessage(CriticalMessage, 0);
+	TerminateTask();
+}
+
+TASK(stop)
+{
+	CancelAlarm(activateA);
+	CancelAlarm(activateB);
+	CancelAlarm(activateC);
+	ShutdownOS(E_OK);
+	TerminateTask();
+}
