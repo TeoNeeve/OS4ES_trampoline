86d0a0323e5c5af09cf2f84e1054c9dda5d9ae99
MatteoMorfino <s345791@studenti.polito.it>
2025-11-26 18:01:35 +0100

    lab 4

--- labsPOLITO/lab04/ex1/ex1.cpp
+++ labsPOLITO/lab04/ex1/ex1.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #include "tpl_os.h"
 #include <avr/wdt.h>
+#define K 5
+#define 
+
+
+DeclareAlarm(a500msec);
+DeclareAlarm(a125msec);
+DeclareAlarm(a100msec);
 
 void setup(void)
 {
+    Serial.begin(115200);
     StartOS(OSDEFAULTAPPMODE);
+    pinMode(A0,INPUT);
 }
 
 void loop(void)
@@ -13,3 +22,6 @@ void loop(void)
         // Task implementations used by the OIL file
     }
 }
+
+
+
