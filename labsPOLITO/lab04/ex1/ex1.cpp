e7f8d9a4c1fe33ef09f8b1d5739461901f2400b8
TeoNeeve <vecchia.teo@gmail.com>
2025-11-26 18:23:17 +0100

    inizio

--- labsPOLITO/lab04/ex1/ex1.cpp
+++ labsPOLITO/lab04/ex1/ex1.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include "tpl_os.h"
 #include <avr/wdt.h>
-#define K 5
-#define 
 
+#define K 5
+#define LED_PIN 13
 
 DeclareAlarm(a500msec);
 DeclareAlarm(a125msec);
@@ -12,8 +12,9 @@ DeclareAlarm(a100msec);
 void setup(void)
 {
     Serial.begin(115200);
-    StartOS(OSDEFAULTAPPMODE);
     pinMode(A0,INPUT);
+    pinMode(LED_PIN, OUTPUT);
+    StartOS(OSDEFAULTAPPMODE);
 }
 
 void loop(void)
@@ -21,6 +22,45 @@ void loop(void)
     while(1){
         // Task implementations used by the OIL file
     }
+}
+
+TASK(TaskS)
+{
+    int A0_valueADC = analogRead(A0);
+    string SensorReadings[K]
+    static int ReadingIndex = 0;
+    if (ReadingIndex < K) {
+        SensorReadings[K] =
+    } else {
+
+    }
+    
+    ReadingIndex++;
+    TerminateTask();
+}
+
+TASK(TaskB)
+{
+    ReadingIndex = 0;
+
+    TerminateTask();
+}
+
+TASK(TaskV)
+{
+    int error
+    int alarm
+    if (error == 1) {
+        CancelAlarm(AlarmBlink);
+        SetRelAlarm(AlarmBlink, 125, 125); // 4 Hz
+    } else if (alarm == 1) {
+        CancelAlarm(AlarmBlink);
+        SetRelAlarm(AlarmBlink, 500, 500); // 1 Hz
+    } else {
+        CancelAlarm(AlarmBlink);
+        digitalWrite(LED_PIN, LOW); // OFF
+    }
+    TerminateTask();
 }
 
 
