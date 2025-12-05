01296495b375cd070ad90208672e36eab3294405
TeoNeeve <vecchia.teo@gmail.com>
2025-12-05 13:31:00 +0100

    last changes, this time for real

--- labsPOLITO/lab04/ex1/ex1.cpp
+++ labsPOLITO/lab04/ex1/ex1.cpp
@@ -103,7 +103,7 @@ TASK(TaskB)
 
 TASK(TaskV)
 {
-    int LED_state = 0;
+    static int LED_state = 0;
     GetResource(SensorRes); 
 
     int Received_alarm = alarm;
@@ -111,21 +111,27 @@ TASK(TaskV)
 
     ReleaseResource(SensorRes);
 
-    if (Received_error == 1 && LED_state != 2) {
-        LED_state = 2;
-        CancelAlarm(AlarmBlink);
-        SetRelAlarm(AlarmBlink, 125, 125); // 4 Hz
-        Serial.println("VELOCE, errore 1"); // DEBUGGING ####################
-    } else if (Received_alarm == 1 && LED_state != 1) {
-        LED_state = 1;
-        CancelAlarm(AlarmBlink);
-        SetRelAlarm(AlarmBlink, 500, 500); // 1 Hz
-        Serial.println("LENTO, errore 0, alarm 1"); // DEBUGGING ############
+    if (Received_error == 1) {
+        if (LED_state != 2) {
+            LED_state = 2;
+            CancelAlarm(AlarmBlink);
+            SetRelAlarm(AlarmBlink, 125, 125); // 4 Hz
+            Serial.println("VELOCE, errore 1"); // DEBUGGING ####################
+        }
+    } else if (Received_alarm == 1) {
+        if (LED_state != 1) {
+            LED_state = 1;
+            CancelAlarm(AlarmBlink);
+            SetRelAlarm(AlarmBlink, 500, 500); // 1 Hz
+            Serial.println("LENTO, errore 0, alarm 1"); // DEBUGGING ############
+        }
     } else {
-        LED_state = 0;
-        CancelAlarm(AlarmBlink && LED_state != 0);
-        digitalWrite(LED_PIN, LOW); // OFF
-        Serial.println("SPENTO, errore e alarm 0"); // DEBUGGING ############
+        if (LED_state != 0) {
+            LED_state = 0;
+            CancelAlarm(AlarmBlink && LED_state != 0);
+            digitalWrite(LED_PIN, LOW); // OFF
+            Serial.println("SPENTO, errore e alarm 0"); // DEBUGGING ############
+        }
     }
 
     TerminateTask();
