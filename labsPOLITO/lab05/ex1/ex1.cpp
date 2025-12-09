5cbee73fa37165e778d81b76373b1a0a3185935c
TeoNeeve <vecchia.teo@gmail.com>
2025-12-09 10:36:04 +0100

    Inizio lab 5

---
+++ labsPOLITO/lab05/ex1/ex1.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "tpl_os.h"
+#include <avr/wdt.h>
+
+void setup(void)
+{
+    //
+}
+
+void loop(void)
+{
+    while(1){
+        // Task implementations used by the OIL file
+    }
+}
